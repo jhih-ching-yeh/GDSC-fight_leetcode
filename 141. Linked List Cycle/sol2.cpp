3f597a56aa13130ebc3f9eb691a67dca105e8181
jhih-ching-yeh <61455441+jhih-ching-yeh@users.noreply.github.com>
2021-10-04 00:46:53 +0800

    Create sol2.cpp

---
+++ 141. Linked List Cycle/sol2.cpp
@@ -0,0 +1,27 @@
+//sol
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        auto slow = head;
+        auto fast = head;
+        
+        while(fast) {
+            if(fast->next == nullptr) return false;
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        
+        return false;
+    }
+};
