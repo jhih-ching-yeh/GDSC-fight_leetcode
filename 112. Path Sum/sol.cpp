44cb37fa0883ec01a9237f3c32b0c546a44ff0a8
jhih-ching-yeh <61455441+jhih-ching-yeh@users.noreply.github.com>
2021-10-13 15:33:36 +0800

    Create sol.cpp

---
+++ 112. Path Sum/sol.cpp
@@ -0,0 +1,27 @@
+//sol
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) return false;
+        if(root->left == nullptr && root->right == nullptr) {
+            if(root->val == targetSum) return true;
+            else return false;
+        }
+        
+        int new_sum = targetSum - root->val;
+        return hasPathSum(root->left, new_sum) || hasPathSum(root->right, new_sum);
+    }
+};
