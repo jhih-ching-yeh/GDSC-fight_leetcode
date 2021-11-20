2105f11d99cfc6faade01d7506dcfa596f0f69d9
jhih-ching-yeh <61455441+jhih-ching-yeh@users.noreply.github.com>
2021-11-20 21:52:37 +0800

    Create sol2.cpp

---
+++ 139. Word Break/sol2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.cbegin(), wordDict.cend()); 
+        return wordBreak(s, dict);
+    }
+    
+    bool wordBreak(string s, unordered_set<string> dict) {
+        if(mem_.count(s)) return mem_[s];
+        if(dict.count(s)) return mem_[s]=true;
+        
+        for(int j=1;j<s.length();j++) {
+            string left = s.substr(0,j);
+            string right = s.substr(j);
+            if(dict.count(right) && wordBreak(left, dict))
+                return mem_[s]=true;
+        }
+        
+        return mem_[s]=false;
+    }
+    
+private:
+    unordered_map<string, bool> mem_;
+};
